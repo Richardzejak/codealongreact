37b0f92bc0bc2df6494b9959f5f1fc28157f8df2
Richardzejak <Richardzejak@hotmail.com>
2021-11-15 15:26:40 +0100

    few stylings

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,43 @@
 import { useNavigate } from 'react-router-dom'
 import RoutingPath from '../routes/RoutingPath'
+import styled from "styled-components"
 
 export const Navigation = () => {
     const navigate = useNavigate()
     return (
         <div>
-            <p onClick={() => navigate(RoutingPath.homeView)}>Home</p>
-            <p onClick={() => navigate(RoutingPath.moviesView)}>Movies</p>
+            <Nav>
+                <NavI onClick={() => navigate(RoutingPath.homeView)}>Home</NavI>
+                <NavI onClick={() => navigate(RoutingPath.moviesView)}>Movies</NavI>
+            </Nav>
         </div>
     )
 }
+
+const Nav = styled.div`
+    background-color: #333;
+    overflow: hidden;
+`
+
+const NavI = styled.a`
+
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+
+    :hover{
+        cursor: pointer;
+        background-color: #ddd;
+        color: black;
+    }
+
+`
+
+const H1 = styled.h1`
+    color: ${props => props.isColored ? props.isColored : 'red'};
+`
+
+
--- src/views/movies/MoviesView.jsx
+++ src/views/movies/MoviesView.jsx
@@ -1,7 +1,19 @@
+import styled from "styled-components"
+
 export const MoviesView = () => {
     return (
         <div>
-            <h1>This is the movies view</h1>
+            <H1>NjetFlix</H1>
+            <H1 isColored="blue">DependsFlix</H1>
+            <H22>PurpleFLix</H22>
         </div>
     )
 }
+
+const H1 = styled.h1`
+    color: ${props => props.isColored ? props.isColored : 'red'};
+`
+
+const H22 = styled.h1`
+color: purple;
+`
